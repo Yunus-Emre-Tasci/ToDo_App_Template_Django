d66e406be23205d056e9f56d7d6f4769d991a151
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-02-18 11:57:34 +0300

    base,list html

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import *
+from .forms import *
 
 # Create your views here.
+def todo_list(request):
+    todos=Todo.objects.all()
+    context={
+        "todos":todos
+    }
+    
+    return render(request,"list.html",context)
